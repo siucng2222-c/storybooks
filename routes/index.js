071960975982f85f77de92b0c2f7127eb10cf8db
xlogger <xloggerster@gmail.com>
2020-06-21 00:07:17 +0800

    Error pages, story models, query DB for stories, dashboard view loop stories

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+const Story = require("../models/Story");
+
 // @desc    Login/Landing page
 // @route   GET /
 router.get("/", ensureGuest, (req, res) => {
@@ -13,12 +15,19 @@ router.get("/", ensureGuest, (req, res) => {
 
 // @desc    Dashboard
 // @route   GET /dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
+router.get("/dashboard", ensureAuth, async (req, res) => {
   // res.send("Dashboard");
 
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories: stories,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render("error/500");
+  }
 });
 
 module.exports = router;
